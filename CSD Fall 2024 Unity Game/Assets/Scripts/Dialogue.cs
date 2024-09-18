ea56c7f0322769da4fa428d39592f78e7daad961
Anshuman Karri <68976092+AnshumanKarri@users.noreply.github.com>
2024-09-18 17:06:26 -0400

    Finished First NPC tasks
    - Added NPC class which implements I_Interactable.cs
    - prompts user to click f to show pop up when in proximity with NPC
    - brings up pop up display and displays dialogue that user can exit by clicking esc

--- CSD Fall 2024 Unity Game/Assets/Scripts/Dialogue.cs
+++ CSD Fall 2024 Unity Game/Assets/Scripts/Dialogue.cs
@@ -10,47 +10,33 @@ using System;
 public class Dialogue : MonoBehaviour
 {
     string dialogue;
-    string[] dialogues;
 
     private RawImage textBackground;
     private TextMeshProUGUI text;
-    private Boolean goToNext;
 
     public Dialogue(string dialogue, RawImage textBackground, TextMeshProUGUI text) { 
         this.dialogue = dialogue;
         this.textBackground = textBackground;
         this.text = text;
+        textBackground.gameObject.SetActive(false);
+        text.gameObject.SetActive(false);
     }
-    public Dialogue(string[] dialogues, RawImage textBackground, TextMeshProUGUI text) {
-        this.dialogues = dialogues;
-        this.textBackground = textBackground;
-        this.text = text;
-    }
-    public void displayDialogue () {
-        if (dialogue != null) {
+
+    // uses Image and text references to show the background and text if the dialogue string is not empty
+    public void displayDialogue()
+    {
+        if (dialogue != null)
+        {
             textBackground.gameObject.SetActive(true);
             text.text = dialogue;
             text.gameObject.SetActive(true);
             Debug.Log(dialogue);
-        } else {
-            textBackground.gameObject.SetActive(true);
-            text.gameObject.SetActive(true);
-            for (int i = 0; i < dialogues.Length; i++) {
-                goToNext = false;
-                text.text = dialogues[i];
-                Debug.Log (dialogues[i]);
+        }
+    }
 
-                while (!goToNext) {
-                    if (Input.GetKeyDown(KeyCode.RightArrow)) {
-                        goToNext = true;
-                    }
-                }
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            textBackground.gameObject.SetActive(false);
-            text.gameObject.SetActive(false);
-            Debug.Log("Esc");
-        }
+    // disables background and text 
+    public void disableDialogue() {
+        textBackground.gameObject.SetActive(false);
+        text.gameObject.SetActive(false);
     }
 }
--- CSD Fall 2024 Unity Game/Assets/Scripts/PlayerController.cs
+++ CSD Fall 2024 Unity Game/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+// TEMPORARY CHARACTER CONTROLLER
 public class PlayerController : MonoBehaviour
-{
-    Dialogue dialogue;
-    [SerializeField] RawImage textBackground;
-    [SerializeField] TextMeshProUGUI text;
+{ 
+    private float moveSpeed = 3;
+    private bool isMoving;
+    private Vector2 input;
 
-    string[] dialogues = { "Hello World", "Interfaces are simple yet confusing", "How tf do you make an interface" };
+    private void Update() {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        dialogue = new Dialogue(dialogues[0], textBackground, text);
-        dialogue.displayDialogue();
+            if (input != Vector2.zero)
+            {
+
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
     }
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+
 }
