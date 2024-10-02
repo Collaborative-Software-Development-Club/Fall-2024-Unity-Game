0d053bad6023fab6867318ef33052f7119193c00
XBY015 <xing.319@buckeyemail.osu.edu>
2024-10-02 19:54:18 -0400

    Hallucination
    finished except for character death, just call the death function in the script under UIController when it's good

--- CSD Fall 2024 Unity Game/Assets/Hallucination/WarningControl.cs
+++ CSD Fall 2024 Unity Game/Assets/Hallucination/WarningControl.cs
@@ -1,24 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class WarningControl : MonoBehaviour
 {
-    public int warningThreshold;
+    public int warningThreshold; //The number of spores required to show the warning sign
+    private int sporeCount;
+    public GameObject warningText;
+    public GameObject sporeObject;
+    private Hallucination hallucination;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(false);
+        // check if sporeObject is assigned
+        if (sporeObject == null)
+        {
+            
+            return;
+        }
+
+        hallucination = sporeObject.GetComponent<Hallucination>(); 
+
+        // check if hallucination is assigned
+        if (hallucination == null)
+        {
+            
+            return;
+        }
+
+        // check if warningText is assigned
+        if (warningText == null)
+        {
+       
+            return;
+        }
+
+        // setting text to be transparent at the first frame
+        warningText.GetComponent<TextMeshProUGUI>().alpha = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GameObject.Find("Fox").GetComponent<Hallucination>().sporeCount == warningThreshold)
+        sporeCount = hallucination.sporeCount;
+
+        if (sporeCount >= warningThreshold)
         {
-            gameObject.SetActive(true);
+            warningText.GetComponent<TextMeshProUGUI>().alpha = 1f; // set text to be not transparent when reaching warningthreshold
         }
-        Debug.Log(GameObject.Find("Fox").GetComponent<Hallucination>().sporeCount == warningThreshold);
+        else
+        {
+            warningText.GetComponent<TextMeshProUGUI>().alpha = 0f; 
+        }
+
     }
 }
