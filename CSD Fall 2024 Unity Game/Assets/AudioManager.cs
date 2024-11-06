9982beaee9ffa16cc62642f92f5c5f686c6e4279
dewsunray <zhou.4782@buckeyemail.osu.edu>
2024-11-06 18:26:23 -0500

    created audio managers for main, maze, and farm
    please merge so it doesn't get deleted

---
+++ CSD Fall 2024 Unity Game/Assets/AudioManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Audio Source")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("Audio Clip")]
+    public AudioClip background;
+
+    void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
