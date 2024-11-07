502206d9699fa608b37028456585211df89df91b
dewsunray <zhou.4782@buckeyemail.osu.edu>
2024-11-07 16:02:44 -0500

    update to the sounds in flowerfield
    added a running in grass sound

--- CSD Fall 2024 Unity Game/Assets/AudioManager.cs
+++ CSD Fall 2024 Unity Game/Assets/AudioManager.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D player;
+
     [Header("Audio Source")]
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
 
     [Header("Audio Clip")]
     public AudioClip background;
+    public AudioClip walkingGrass;
 
+    // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
         musicSource.clip = background;
         musicSource.Play();
     }
@@ -21,6 +26,21 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!player.velocity.Equals(Vector2.zero))
+        {
+            SFXSource.Play();
+        }
+        else
+        {
+            SFXSource.Pause();
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SFXSource.clip = walkingGrass;
+        
         
     }
 }
