994ebb8e1c1fb722f63ec178f4322ce1c18bafac
csandcastle <csandcastle119@gmail.com>
2024-12-02 00:50:41 -0500

    Added basic scene transitions
    In prefabs
    
    Added global prefab folder to store player prefab and sceneTransitionCollider prefab
    
    Created sceneTransitionCollider prefab which uses the scene transition script in order to transport the player to another scene when a collider is triggered
    
    In Scripts
    
    Added GlobalScripts Folder to host the scene transition script
    
    Created sceneTransition Script

---
+++ CSD Fall 2024 Unity Game/Assets/Scripts/GlobalScripts/SceneTransition.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Name of scene to transition to")]
+    private string sceneName;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
